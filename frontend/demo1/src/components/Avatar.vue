216fafae498e5cddd395a697fb126c4d71d95f98
alantu <alan020113@gmail.com>
2023-05-09 22:11:32 +0800

    增加了状态管理

---
+++ frontend/demo1/src/components/Avatar.vue
@@ -0,0 +1,32 @@
+<template>
+  <n-avatar></n-avatar>
+</template>
+
+<script setup>
+import { ref, reactive, getCurrentInstance } from "vue";
+import { useRouter, useRoute } from "vue-router";
+const router = useRouter();
+const route = useRoute();
+const { proxy } = getCurrentInstance();
+
+const props = defineProps({
+  userID: {
+    type: Number,
+  },
+  addLink: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const GoToUser = () => {
+  if (props.addLink) {
+    router.push({
+      name: "User",
+      params: {
+        id: props.userID,
+      },
+    });
+  }
+};
+</script>
