6d9e8c0f5d2dd8db2e11d7c1e98f4e0b37898d0a
emt0re0 <1320598294@qq.com>
2023-05-11 17:31:35 +0800

    11

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -18,12 +18,12 @@ async def get_me(user: schemas.User = Depends(security.get_current_user)):
 
 
 # 根据id获取部分用户信息 json格式
-@router.post("/{user_id.id}")
+@router.get("/{user_id}")
 async def get_user(
-    user_id: schemas.UserDelete,
+    user_id: int,
     db: Session = Depends(get_db),
 ):
-    db_user = crud.get_user(db, user_id=user_id.id)
+    db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return {
@@ -31,6 +31,7 @@ async def get_user(
         "username": db_user.username,
         "email": db_user.email,
         "avatar_url": db_user.avatar_url,
+        "introduction": db_user.introduction,
         "is_active": db_user.is_active,
     }
 
