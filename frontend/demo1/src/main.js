5969764cd25f37f5d13c5401c5fe242278715a74
alantu <alan020113@gmail.com>
2023-05-23 17:46:37 +0800

    就这样

--- frontend/demo1/src/main.js
+++ frontend/demo1/src/main.js
@@ -8,7 +8,7 @@ import naive from "naive-ui";
 import ElementPlus from "element-plus";
 
 import "./assets/base.less";
-import "./assets/sspai.less";
+//import "./assets/sspai.less";
 
 import "element-plus/dist/index.css";
 
