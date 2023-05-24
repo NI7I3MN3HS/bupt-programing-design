01e356f976a82e9d42225d9afbb37d3d4e2eabdc
alantu <alan020113@gmail.com>
2023-05-24 23:10:06 +0800

    系统通知卡片已完成

--- frontend/demo1/src/views/Notice/NoticeCard.vue
+++ frontend/demo1/src/views/Notice/NoticeCard.vue
@@ -1,3 +1,21 @@
 <template>
-  <n-card> <template #header>用户</template>内容 </n-card>
+  <n-card>
+    <template #header>系统通知</template>
+    {{ data.content }}
+  </n-card>
 </template>
+
+<script setup>
+import axios from "axios";
+import { toRefs } from "vue";
+
+const props = defineProps({
+  //子组件接收父组件传递过来的值
+  data: {
+    type: Object,
+  },
+});
+const { data } = toRefs(props);
+
+console.log(data.value);
+</script>
--- frontend/demo1/src/views/Notice/index.vue
+++ frontend/demo1/src/views/Notice/index.vue
@@ -6,17 +6,48 @@
       </div>
       <n-divider />
       <el-tabs v-model="activeName" class="demo-tabs" stretch="true">
-        <el-tab-pane label="评论" name="first"><NoticeCard /></el-tab-pane>
-        <el-tab-pane label="系统" name="second">Config</el-tab-pane>
+        <el-tab-pane label="系统" name="first">
+          <n-space vertical>
+            <div v-for="item in notices"><NoticeCard :data="item" /></div>
+          </n-space>
+        </el-tab-pane>
       </el-tabs>
     </n-space>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import axios from "axios";
+import { onBeforeMount, ref } from "vue";
+import useAuthStore from "../../stores/modules/AuthStore";
+import useUserStore from "../../stores/modules/UserStore";
 import NoticeCard from "./NoticeCard.vue";
 
 const activeName = ref("first");
+
+const authStore = useAuthStore();
+const userStore = useUserStore();
+
+// 通知列表
+const notices = ref([]);
+
+const UserClient = axios.create({
+  baseURL: "http://localhost:8000",
+  timeout: 10000,
+  headers: {
+    Accept: "application/json",
+    Authorization: `Bearer ${authStore.token}`,
+  },
+});
+
+// 获取通知列表
+const getNotices = async () => {
+  const res = await UserClient.get("/notification/all");
+  notices.value = res.data;
+};
+
+onBeforeMount(() => {
+  getNotices();
+});
 </script>
 <style lang="less">
 .demo-tabs > .el-tabs__content {
