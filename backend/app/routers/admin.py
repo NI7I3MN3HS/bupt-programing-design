3eee6e0eccd7a139e9edba3e51fc234043f1883a
emt0re0 <1320598294@qq.com>
2023-05-25 16:54:03 +0800

    管理员登录和用户更新判断

--- backend/app/routers/admin.py
+++ backend/app/routers/admin.py
@@ -1,7 +1,14 @@
 # 管理员模块
-
+from datetime import timedelta
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
+from ..core.security import (
+    verify_password,
+    create_access_token,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+)
+
 from ..sql_app import models, schemas, crud
 from ..sql_app.database import get_db
 from ..core import security
@@ -9,6 +16,37 @@ from ..core import security
 router = APIRouter(prefix="/admin", tags=["admin"])
 
 admin_password = "admin"
+
+
+# 管理员登录
+@router.post("/", response_model=schemas.Token)
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    # 验证用户名和密码是否正确
+    user = crud.get_user_by_username(db, username=form_data.username)
+
+    email = crud.get_user_by_email(db, email=form_data.username)
+    if not user and not email:
+        raise HTTPException(status_code=400, detail="Incorrect email or username")
+    if user:
+        if not verify_password(form_data.password, user.hashed_password):
+            raise HTTPException(
+                status_code=400, detail="Incorrect username or password"
+            )
+        if user.is_admin == False:
+            raise HTTPException(status_code=400, detail="Not admin")
+    if email:
+        if not verify_password(form_data.password, email.hashed_password):
+            raise HTTPException(status_code=400, detail="Incorrect email or password")
+        if email.is_admin == False:
+            raise HTTPException(status_code=400, detail="Not admin")
+        form_data.username = email.username
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": form_data.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 # 注册管理员用户
